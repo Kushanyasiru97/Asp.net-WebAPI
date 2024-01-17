44144ee611bec2e7dcae46e914d33c272da49af3
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2024-01-17 17:40:57 +0530

    HTTP methods created

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somthingWentWrong = true;
+            if (somthingWentWrong)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
